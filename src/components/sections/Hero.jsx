62fdd73ad16b7356980d3f468e8c27ac314aeaaf
joshuafronda <21-07245@g.batstate-u.edu.ph>
2025-07-03 08:44:53 +0800

    Update: Resume/CV

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -141,7 +141,7 @@ const Hero = () => {
               </a>
               <div className="gap-4 ml-4">
               <a
-                href="https://drive.google.com/file/d/1B3OBfNcR-cNtmix8ZZUNlGiGx-ZqoP4X/view?usp=sharing"
+                href="https://drive.google.com/file/d/1HFSbfqmgXebdix5-IGPpv5KAERFb0w8S/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-400 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400 text-sm font-medium transition-colors relative group"
